65f3ad4e5f3c8db4a581780a79fd802465626cbc
Amar Agrawal <159050763+amarjec@users.noreply.github.com>
2024-03-30 11:58:27 +0530

    Create Minimum element in BST.java

---
+++ Minimum element in BST.java
@@ -0,0 +1,15 @@
+class Solution {
+    // Function to find the minimum element in the given BST.
+    int minValue(Node root) {
+        // code here
+        if(root == null){
+            return -1;
+        }
+        if(root.left == null){
+            return root.data;
+        }
+        else{
+            return minValue(root.left);
+        }
+    }
+}
