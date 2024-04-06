84af813acffbecb57d51a80ccbf14ad7e843fe50
Amar Agrawal <159050763+amarjec@users.noreply.github.com>
2024-04-06 12:27:35 +0530

    Create 6th April.java

---
+++ 6th April.java
@@ -0,0 +1,9 @@
+class Solution {
+    // Function to count number of ways to reach the nth stair
+    // when order does not matter.
+    Long countWays(int n) {
+        // your code here
+         long ans=n/2+1;
+        return ans;
+    }
+}
