4d9a6a39ab4d78cbf6c4455c0840d08406b4eb5a
Amar Agrawal <159050763+amarjec@users.noreply.github.com>
2024-04-29 22:25:22 +0530

    Create 29th April.java

---
+++ 29th April.java
@@ -0,0 +1,31 @@
+class Solution
+{
+    /*You are required to complete this method*/
+    Node delete(Node head, int k)
+    {
+	// Your code here	 
+	  if(head == null || head.next== null ||k==0 )
+        return head;
+        
+        if(k==1)
+            return null;
+	    
+	    int nodeIndex = 1;
+	    Node p = head;
+	    int counter = 1;
+	    while(p!=null && p.next!=null){
+	        if(counter == k-1){
+	       
+	            p.next = p.next.next;
+	            counter = 1;
+	     
+	        }
+	        else{
+	            counter++;
+	        }
+	        p=p.next;
+	        
+	    }
+	    return head;
+    }
+}
